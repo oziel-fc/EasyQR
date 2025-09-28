5332b4f5957a910a0e5ea09b024fc89614cbf82d
oziel-fc <ozielfc.oficial@gmail.com>
2025-09-28 02:32:01 -0300

    Create a fonts picker and stylizing it

--- easyqr/src/components/Main/FontsPicker.tsx
+++ easyqr/src/components/Main/FontsPicker.tsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
+import styles from "./FontsPicker.module.css";
 
 const FontSelector = () => {
   const [selectedFont, setSelectedFont] = useState("");
   const fonts = ["Arial", "Courier New", "Georgia", "Times New Roman", "Verdana"];
 
   return (
-    <div>
-      <select
+    <div className={styles.font_picker}>
+      <span>Font</span>
+      <select className={styles.picker}
         value={selectedFont}
         onChange={(e) => setSelectedFont(e.target.value)}
+        style={{ fontFamily: selectedFont }}
       >
-        <option value="">Selecione uma fonte</option>
+        <option value="" >Selecione uma fonte</option>
         {fonts.map((font, index) => (
           <option
             key={index}
             value={font}
-            style={{ fontFamily: font }} // cada item com a própria fonte
+            style={{ fontFamily: font }} 
           >
             {font}
           </option>
