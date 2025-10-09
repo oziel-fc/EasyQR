fa4164a406929334f071cc2017dd3821438011bb
oziel-fc <ozielfc.oficial@gmail.com>
2025-10-09 17:09:36 -0300

    Finishing the about section and making the page more responsive

--- easyqr/src/components/About/About.tsx
+++ easyqr/src/components/About/About.tsx
@@ -7,18 +7,38 @@ const About = () => {
   return (
     <section className={styles.about}>
         <div className={styles.about_container}>
-            <div className={styles.container_left}></div>
-            <div className={styles.container_right}></div>
-            {/* <div style={{width: "300px", height: "300px"}}>
-                <img src={image_qr} alt="" />
+            <div className={styles.about_title}><h1 style={{fontSize: "20px"}}>About EasyQR</h1></div>
+            <div className={styles.about_content}>
+                {/* left side */}
+                <div className={styles.side_left}>
+                    <div className={styles.about_text}>
+                        <p>EasyQR is a free and intuitive tool that lets you generate QR Codes in seconds. With it, 
+                        you can easily create customized codes for links, texts, numbers, contacts, Wi-Fi, and much more, all with just a few clicks.</p>
+                    </div>
+                    <div className={styles.about_image}>
+                        <img src={image_design} alt="" />
+                    </div>
+                    <div className={styles.about_text}>
+                        <p>Finally, you can download and share the generated QR code in different formats. This project is part of 
+                        my personal portfolio, you can find more information in the footer.</p>
+                    </div>
+                </div>
+                {/* right side */}
+                <div className={styles.side_right}>
+                    <div className={styles.about_image}>
+                        <img src={image_qr} alt="image about"/>
+                    </div> 
+                    <div className={styles.about_text}>
+                        <p>Quickly create fully customizable QR Codes.
+                        Choose a background color, main color, maybe a custom logo. <br/>Customize the code to your style.</p>
+                    </div>
+                    <div className={styles.about_image}>
+                        <img src={image_download} alt="" />
+                    </div>  
+                </div>
             </div>
-            <div style={{width: "300px", height: "300px"}}>
-                <img src={image_design} alt="" />
-            </div>
-            <div style={{width: "300px", height: "300px"}}>
-                <img src={image_download} alt="" />
-            </div> */}
         </div>
+        
     </section>
   )
 }
--- easyqr/src/components/Description/ToolDescription.tsx
+++ easyqr/src/components/Description/ToolDescription.tsx
@@ -8,18 +8,18 @@ const Description = () => {
   return (
     <section className={styles.description}>
       <div className={styles.description_container}>
-        <h1 style={{fontSize: "30px"}}>EasyQR: Create your Free QR Code</h1>
-        <p style={{color: "#adadad"}}>Customize with your style in just 3 steps</p>
+        <h1 className={styles.description_title}>EasyQR: Create your Free QR Code</h1>
+        <p className={styles.description_subtext}>Customize with your style in just 3 steps</p>
         <div className={styles.tutorial_cards}>
+          {/* Card 1 */}
           <div className={styles.hover_change}>
-
             <div className={styles.tool_description}>
               <img src={icon_url} alt="url icon" />
               <h3 style={{fontSize: "24px"}}>Choose Your Content</h3>
               <p style={{color: "rgb(173, 173, 173)"}}>Enter any text in the input box to create the QR Code.</p>
             </div>
           </div>
-          
+          {/* Card 2 */}
           <div className={styles.hover_change}>
             <div className={styles.tool_description}>
               <img src={icon_customize} alt="customize icon" />
@@ -27,6 +27,7 @@ const Description = () => {
               <p style={{color: "rgb(173, 173, 173)"}}>Customize your QR Code in the best way, change colors and choose different logos.</p>
             </div>
           </div>
+          {/* Card 3 */}
           <div className={styles.hover_change}>
             <div className={styles.tool_description}>
               <img src={icon_download} alt="download icon" />
